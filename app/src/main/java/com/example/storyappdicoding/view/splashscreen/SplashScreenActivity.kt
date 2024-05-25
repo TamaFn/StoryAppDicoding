3a6e7fe1b3268f96852a9065d18143b1678e0b50
TamaFn <sandya.fransisna@gmail.com>
2024-05-26 01:12:45 +0700

    Add SplashScreen and Icon App

---
+++ app/src/main/java/com/example/storyappdicoding/view/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,32 @@
+package com.example.storyappdicoding.view.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.storyappdicoding.R
+import com.example.storyappdicoding.databinding.ActivitySplashScreenBinding
+import com.example.storyappdicoding.view.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
